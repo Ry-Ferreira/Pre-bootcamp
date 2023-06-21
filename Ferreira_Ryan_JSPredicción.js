9e524d3edef7a63651cb7a6c0517260d77cf25d4
Ry-Ferreira <63878456+Ry-Ferreira@users.noreply.github.com>
2023-06-21 19:14:27 -0300

    Add files via upload

---
+++ Ferreira_Ryan_JSPredicción.js
@@ -0,0 +1,12 @@
+// predicción 1
+El console.log imprimirá "Nací en 1980"
+
+// predicción 2
+El console.log imprimirá siendo "EntradaAñoNacimiento" = 1980, el console.log imprimirá "Nací en 1980"
+
+// predicción 3
+
+la funcion "add", imprimirá en pantalla: "¡Sumando números!"
+                                         "num1 is: 10"
+                                         "num2 is: 20"
+                                         "30"
